9fe2bef7fcd4398d01f05e007eea3faf10f2873d
Aieychan01 <87831895+Aieychan01@users.noreply.github.com>
2021-10-13 23:23:09 -0300

    envio da atividade

---
+++ src/Mensagem.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+
+export const Mensagem= () =>(<p>Há pessoas que choram por saber que as rosas têm espinho. Há outras que sorriem por saber que os espinhos têm rosas!</p>)
+  
